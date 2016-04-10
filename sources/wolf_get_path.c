31c9c176641dae5474d76f73bc1083ec95cae10b
Clement CHAMEYRAT <cchameyr@e3r12p6.42.fr>
2016-04-10 12:17:09 +0200

    partie get map coder, commencement des checks sur la map

--- sources/wolf_get_path.c
+++ sources/wolf_get_path.c
@@ -6,33 +6,55 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/08 15:39:55 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/04/09 12:59:55 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/04/10 12:15:34 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/header.h"
 
-static void		ft
+static void		ft_line_to_tab(t_lstline *list, int nb_line, t_wolf3d *w3d)
+{
+	char	**str;
+	int		y;
+	int		x;
+	int		len;
+
+	w3d->map = (int **)ft_memalloc(sizeof(int *) * (nb_line + 1));
+	y = -1;
+	while (++y < nb_line)
+	{
+		str = ft_strsplit(list->line, ' ');
+		len = ft_memlen((void **)str);
+		w3d->map[y] = (int *)ft_memalloc(sizeof(int) * (len + 1));
+		x = -1;
+		while (str[++x])
+			w3d->map[y][x] = ft_atoi(str[x]);
+		while (x--)
+			ft_memdel((void **)&str[x]);
+		ft_memdel((void **)str);
+		list = list->next;
+	}
+}
 
 void			get_map_path(t_wolf3d *w3d, const int fd)
 {
 	char		*line;
+	int			nb_line;
 	t_lstline	*list;
 
 	line = NULL;
 	list = NULL;
-	w3d->nb_line = 0;
+	nb_line = 0;
 	while (get_next_line(fd, &line) > 0)
 	{
-		//		if (ft_check_line(line) == 0)
-		if (0)
-		{
-			ft_lstline_del(list);
-			ft_exit_wolf3d(w3d, 1);
-		}
 		list = ft_add_lstline(list, line);
-		w3d->nb_line++;
+		nb_line++;
 	}
-	ft_line_to_tab(list, w3d->nb_line, w3d);
+	if (ft_check_map(list) == 0)
+	{
+		ft_lstline_del(list);
+		ft_exit_wolf3d(w3d, 1);
+	}
+	ft_line_to_tab(list, nb_line, w3d);
 	ft_lstline_del(list);
 }
