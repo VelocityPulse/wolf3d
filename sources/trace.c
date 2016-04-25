9666611ecf4ff7f46856b3a8ef6e8b3a63dd9d88
Velocitypulse <velocitypulse@outlook.fr>
2016-04-25 13:19:08 +0200

    ajout d'une fonction de racourcissement

--- sources/trace.c
+++ sources/trace.c
@@ -6,13 +6,31 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/20 17:11:57 by                   #+#    #+#             */
-/*   Updated: 2016/04/23 16:44:23 by                  ###   ########.fr       */
+/*   Updated: 2016/04/25 13:18:10 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/header.h"
 
-void	ft_trace(t_wolf3d *w3d, int line_start, int line_end, int x)
+static int		ft_trace_tex_x(t_raycasting *r, int size_x)
+{
+	double	wall_x;
+	int		tex_x;
+
+	if (r->side == 0)
+		wall_x = r->ray_posy + r->perp_wall_dist * r->ray_diry;
+	else
+		wall_x = r->ray_posx + r->perp_wall_dist * r->ray_dirx;
+	wall_x -= floor(wall_x);
+	tex_x = (int)(wall_x * (double)size_x);
+	if (r->side == 0 && r->ray_dirx > 0)
+		tex_x = size_x - tex_x - 1;
+	if (r->side == 1 && r->ray_diry < 0)
+		tex_x = size_x - tex_x - 1;
+	return (tex_x);
+}
+
+void			ft_trace(t_wolf3d *w3d, int line_start, int line_end, int x)
 {
 	t_raycasting	*r;
 	t_texture		*t;
@@ -20,7 +38,6 @@ void	ft_trace(t_wolf3d *w3d, int line_start, int line_end, int x)
 	int				size_x;
 	int				size_y;
 	int				d;
-	double			wall_x;
 	int				tex_x;
 	int				line_height;
 	int				 color;
@@ -28,31 +45,26 @@ void	ft_trace(t_wolf3d *w3d, int line_start, int line_end, int x)
 	r = &w3d->r;
 	t = &w3d->t;
 	line_height = r->line_height;
-	if (r->side == 0)
-		wall_x = r->ray_posy + r->perp_wall_dist * r->ray_diry;
-	else
-		wall_x = r->ray_posx + r->perp_wall_dist * r->ray_dirx;
-	wall_x -= floor(wall_x);
+
 	if (r->val >= 30 && r->val <= 37)
 	{
+		int *colon;
+		int	width;
+		long int	position;
+		void		*data;
+
 		size_x = t->list_img[r->val - 30]->size.x;
 		size_y = t->list_img[r->val - 30]->size.y;
-		tex_x = (int)(wall_x * (double)size_x);
-		if (r->side == 0 && r->ray_dirx > 0)
-			tex_x = size_x - tex_x - 1;
-		if (r->side == 1 && r->ray_diry < 0)
-			tex_x = size_x - tex_x - 1;
+		tex_x = ft_trace_tex_x(r, size_x);
+		colon = t->list_text[r->val - 30][tex_x];
 		if (w3d->key_squat != 1)
 		{
 			line_start += 120;
 			line_end += 120;
 		}
 		y = line_start - 1;
-		int *colon = t->list_text[r->val - 30][tex_x];
-		int	width;
-		long int	position;
-		void		*data;
-		position = (y * w3d->mlx->mlx_img->width) + (x * w3d->mlx->mlx_img->octet);
+		position = ((y - (w3d->key_squat == 1 ? 0 : 120)) * w3d->mlx->mlx_img->width) +
+			(x * w3d->mlx->mlx_img->octet);
 		data = w3d->mlx->mlx_img->data;
 		width = w3d->mlx->mlx_img->width;
 		while (++y < line_end)
@@ -61,20 +73,21 @@ void	ft_trace(t_wolf3d *w3d, int line_start, int line_end, int x)
 			int tex_y;
 			tex_y = ((d * size_y) / line_height) / 256;
 			color = colon[tex_y];
-			if (r->side == 1)
-			{
-				t_rgb c;
-				c = ft_get_rgb(color);
-				c.r /= 2;
-				c.g /= 2;
-				c.b /= 2;
-				color = ft_get_hexa(c);
-			}
+			/*			if (r->side == 1)
+						{
+						t_rgb c;
+						c = ft_get_rgb(color);
+						c.r /= 2;
+						c.g /= 2;
+						c.b /= 2;
+						color = ft_get_hexa(c);
+						}
+						*/
 			if (!(position < 0 || position > w3d->mlx->mlx_img->max_size))
-			ft_memcpy(data + position, &color, w3d->mlx->mlx_img->octet);
+				ft_memcpy(data + position, &color, w3d->mlx->mlx_img->octet);
 			position += width;
-//			ft_draw_pixel(w3d->mlx, color, ft_make_pt(x, y -
-//			(w3d->key_squat == 1 ? 0 : 120)));
+			//			ft_draw_pixel(w3d->mlx, color, ft_make_pt(x, y -
+			//			(w3d->key_squat == 1 ? 0 : 120)));
 		}
 	}
 	else
