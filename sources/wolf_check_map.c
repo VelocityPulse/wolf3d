7c1dee14e12590d0e13aebc0f4ae843e55242252
Velocitypulse <velocitypulse@outlook.fr>
2016-04-30 14:51:51 +0200

    modification de la fonction check_map. retrait du message d'erreur lorsqu'il y a plus d'un espace a la suite. explications dans les commentaires

--- sources/wolf_check_map.c
+++ sources/wolf_check_map.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/10 12:16:02 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/04/20 17:55:46 by                  ###   ########.fr       */
+/*   Updated: 2016/04/30 14:49:53 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,6 @@
 
 static int		ft_check_spawn(char *n, int *spawn, t_wolf3d *w3d, t_pt p)
 {
-	static int		space = 0;
-
-	if (*n == ' ')
-		space++;
-	else
-		space = 0;
-	if (space > 1)
-	{
-		ft_putstr("\nERROR:\nToo much space ");
-		return (ft_error_line(p, 1));
-	}
 	if (*n == 'x' || *n == 'X')
 	{
 		*spawn = *spawn + 1;
@@ -37,25 +26,33 @@ static int		ft_check_spawn(char *n, int *spawn, t_wolf3d *w3d, t_pt p)
 int				ft_check_map(t_wolf3d *w3d, t_lstline *l, int max_x, int s)
 {
 	t_pt		p;
-	int			cx;
+	t_pt		cx;
 
 	p.y = 0;
+	cx.y = 0;
 	while (l && (++p.y) && (p.x = -1))
 	{
-		cx = 1;
+		cx.x = 1;
 		while (l->line[++p.x])
 		{
-			if (l->line[p.x] == ' ')
-				cx++;
-			if (!ft_check_spawn(&l->line[p.x], &s, w3d, ft_make_pt(cx, p.y)))
+			if (l->line[p.x] == ' ' && !(cx.y == ' '))
+				cx.x++;
+			if (!ft_check_spawn(&l->line[p.x], &s, w3d, ft_make_pt(cx.x, p.y)))
 				return (0);
 			if (!(l->line[p.x] >= '0' && l->line[p.x] <= '9') &&
 					l->line[p.x] != ' ')
 				return (ft_error_line(p, 0));
 			if (ft_check_pos_spawn(w3d, &max_x, p, s) == 0)
 				return (0);
+			cx.y = l->line[p.x];
 		}
 		l = l->next;
 	}
 	return (ft_error(s, p.y, max_x));
 }
+
+/*
+** structure x;y p is the position in the map.
+** cx.x is the compter of real number
+** cx.y is the tmp for the last character in the tab
+*/
