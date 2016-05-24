427850bf84745a486e341c1a1da41c2fa036a0cd
Clement CHAMEYRAT <cchameyr@e2r10p19.42.fr>
2016-05-24 11:33:08 +0200

    mise a jour des fichiers delta time

--- headers/delta_time.h
+++ headers/delta_time.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/05/03 12:14:13 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/05/03 12:15:08 by cchameyr         ###   ########.fr       */
+/*   Created: 2016/04/08 15:52:06 by cchameyr          #+#    #+#             */
+/*   Updated: 2016/05/24 11:29:27 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ typedef struct	s_delta
 	double			fps;
 }				t_delta;
 
-void			get_deltatime(t_delta *d, const int fps_mode);
+void			get_delta(t_delta *d, const int fps_mode);
 void			start_delta(t_delta *d);
 void			end_delta(t_delta *d);
 
