30c5de79208dd2a29c661274600a8fa16d619978
Clement CHAMEYRAT <cchameyr@e2r10p19.42.fr>
2016-05-24 11:15:01 +0200

    changement du free de list_img

--- sources/clear_texture.c
+++ sources/clear_texture.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/03 11:59:40 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/05/03 11:59:50 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/05/24 11:14:45 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,6 @@ void			ft_clear_list_textures(t_texture *t, int ***list_text)
 		ft_memdel((void **)list_text[i]);
 	}
 	free(list_text);
+	ft_memdel((void **)t->list_img);
 	ft_putstr("Texture tab :\tCLEAR\n");
 }
--- sources/exit_wolf.c
+++ sources/exit_wolf.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/09 11:29:32 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/05/24 11:09:54 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/05/24 11:13:39 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,6 @@ static void		ft_exit_3(t_wolf3d *w3d)
 		ft_memdel((void **)&w3d->map[i]);
 	ft_memdel((void **)w3d->map);
 	ft_putstr("Map tab :\tCLEAR\n");
-	free(w3d->t.list_img);
 	ft_memdel((void **)&w3d->len_map);
 	ft_memdel((void **)&w3d);
 	ft_putstr("Wolf3d :\tCLEAR\n\n");
